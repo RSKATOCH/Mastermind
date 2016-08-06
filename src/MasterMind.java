bdef2160450e8e86a4f2a9fcdcd5258cca2a4b40
Katoch <rkatoch@paypalcorp.com>
2016-08-06 09:43:24 +0530

    Main function

--- src/MasterMind.java
+++ src/MasterMind.java
@@ -3,23 +3,26 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class MasterMind {
 
 	private Set<String> dictionary = new HashSet<String>();
 	private String gameWord;
-	
+	/*Function to get sorted strings */
 	private String getSorted(String word) {
 		char[] wordArray = word.toCharArray();
 		Arrays.sort(wordArray);
 		return String.valueOf(wordArray);
 	}
 	
+	/*Function to get read file*/
 	public void readFile(String filePath, int length) throws Exception {
 		getWordSet((ArrayList<String>) Files.readAllLines(Paths.get(filePath)), length);
 	}
 	
+	/*Function to create dictionary*/
 	private void getWordSet(ArrayList<String> readAllLines, int length) {
 		// TODO Auto-generated method stub
 		for(String line  : readAllLines) {
@@ -28,7 +31,8 @@ public class MasterMind {
 			}
 		}
 	}
-
+	
+	/*Dictionary contains only unique characters*/
 	private boolean hasUniqueLetters(String line) {
 		for(char ch : line.toCharArray()) {
 			if(line.indexOf(ch) != line.lastIndexOf(ch)) {
@@ -40,6 +44,7 @@ public class MasterMind {
 	
 	private String processWord(String word) {
 		int numOfCommon = commonCharsCount(word);
+		
 		return null;
 	}
 	
@@ -85,9 +90,17 @@ public class MasterMind {
 	public static void main(String[] args) throws NumberFormatException, Exception {
 		// TODO Auto-generated method stub
 		MasterMind game = new MasterMind();
-		
+		Scanner sc = new Scanner(System.in);
 		game.readFile(args[0], Integer.parseInt(args[1]));
-		System.out.println(game.dictionary);
+		game.generateGameWord();
+		String word="";
+		do{
+			/*User moves first*/
+			word = sc.next();
+			System.out.println();
+			
+		}while(game.isWin(word));
+			
 		//Scanner sc = new Scanner(System.in);
 		//System.out.println(choose);
 		//System.out.println(Permutations.permutationFinder("ABC"));
